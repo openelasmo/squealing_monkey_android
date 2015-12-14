0c9f45c7edfe759cde79391d48bce79d5cf0f94d
Kieran <kierantw@hotmail.co.uk>
2015-12-14 13:36:28 +0000

    Added Map & Nav Drawer

--- app/src/main/java/com/example/pk/openvoidjobs/LandingActivity.java
+++ app/src/main/java/com/example/pk/openvoidjobs/LandingActivity.java
@@ -1,30 +1,66 @@
 package com.example.pk.openvoidjobs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+//import com.example.pk.openvoidjobs.DownloadWebpageTask.AsyncResponse;
 
 /**
  * Created by PK on 04/12/2015.
  */
-public class LandingActivity extends AppCompatActivity {
+
+
+public class LandingActivity extends AppCompatActivity /*implements AsyncResponse*/{
+
+    public static String webpageExample = "";
+    @Bind(R.id.link_viewjobs) TextView _viewjobsLink;
+
+    String webpageURL = "http://52.19.81.247:8080/api/users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        ButterKnife.bind(this);
+
         String username = getIntent().getStringExtra("Username");
 
         TextView tv = (TextView)findViewById(R.id.TVusername);
         tv.setText(username);
 
-        DownloadWebpageExample example = new DownloadWebpageExample();
+        _viewjobsLink.setOnClickListener(new View.OnClickListener() {
 
-        //String webpage = example.toString();
+            @Override
+            public void onClick(View v) {
+                //Start ViewJobsMapActivity activity
+                Intent intent = new Intent(getApplicationContext(), ViewJobsMapActivity.class);
+                startActivity(intent);
+            }
 
-        //TextView webpageTV = (TextView) findViewById(R.id.restJSONget);
-        //webpageTV.setText(webpage);
+        });
 
+
+
+        DownloadWebpageTask example = new DownloadWebpageTask();
+
+        //execute the async task
+        example.execute(webpageURL);
+
+        // http://stackoverflow.com/questions/9963691/android-asynctask-sending-callbacks-to-ui
+
+        String webpage = "";
+        webpage = webpageExample;
+        Log.v("Webpage", webpage);
+
+        TextView webpageTV = (TextView) findViewById(R.id.restJSONget);
+        webpageTV.setText(webpage);
     }
 
 }
+
---
+++ app/src/main/java/com/example/pk/openvoidjobs/ViewJobsMapActivity.java
@@ -0,0 +1,193 @@
+package com.example.pk.openvoidjobs;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.view.GravityCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by Kieran on 09/12/2015.
+ */
+public class ViewJobsMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private LatLng defaultLatLng = new LatLng(51.501531, -0.141901);
+    private int zoomLevel = 10;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mPlanetTitles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mTitle = mDrawerTitle = getTitle();
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Add a few dummy markers, and move the camera to start at default position.
+
+        map.addMarker(new MarkerOptions().position(defaultLatLng).title("Queen lives here"));
+
+        LatLng kingston = new LatLng(51.413861, -0.299936);
+        map.addMarker(new MarkerOptions()
+                .position(kingston)
+                .title("muppet"));
+
+        LatLng streatham = new LatLng(51.419959, -0.128017);
+        map.addMarker(new MarkerOptions()
+                .position(streatham)
+                .title("Super cool guy")
+                .snippet("I'm so ronery"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLatLng, zoomLevel));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_map, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.action_websearch:
+                // create intent to perform web search for this planet
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, getSupportActionBar().getTitle());
+                // catch event that there's no activity to handle intent
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // what to do when item selected from navigation drawer
+
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
