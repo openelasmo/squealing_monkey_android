49045ad15c3aad489433e5e5b7318960ecd41911
pkaneri <pkaneri@gmail.com>
2015-12-21 18:10:05 +0000

    Added Volley class to the project

--- app/src/main/java/com/example/pk/openvoidjobs/LandingActivity.java
+++ app/src/main/java/com/example/pk/openvoidjobs/LandingActivity.java
@@ -1,11 +1,29 @@
 package com.example.pk.openvoidjobs;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,12 +34,26 @@ import butterknife.ButterKnife;
  */
 
 
-public class LandingActivity extends AppCompatActivity /*implements AsyncResponse*/{
+public class LandingActivity extends AppCompatActivity {
 
     public static String webpageExample = "";
     @Bind(R.id.link_viewjobs) TextView _viewjobsLink;
 
-    String webpageURL = "http://52.19.81.247:8080/api/users";
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+
+    private static final String QUERY_URL = "http://52.19.81.247:8080/api/users";
+    ListView mainListView;
+
+    ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,22 +77,92 @@ public class LandingActivity extends AppCompatActivity /*implements AsyncRespons
 
         });
 
+        btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
 
 
-        DownloadWebpageTask example = new DownloadWebpageTask();
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
 
-        //execute the async task
-        example.execute(webpageURL);
+            @Override
+            public void onClick(View v) {
+                // making json array request
+                makeJsonArrayRequest();
+            }
+        });
 
-        // http://stackoverflow.com/questions/9963691/android-asynctask-sending-callbacks-to-ui
-
-        String webpage = "";
-        webpage = webpageExample;
-        Log.v("Webpage", webpage);
-
-        TextView webpageTV = (TextView) findViewById(R.id.restJSONget);
-        webpageTV.setText(webpage);
     }
 
+    /**
+     * Method to make json array request where response starts with [
+     * */
+    private void makeJsonArrayRequest() {
+
+        showpDialog();
+
+        JsonArrayRequest req = new JsonArrayRequest(QUERY_URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response
+                                        .get(i);
+
+                                String name = person.getString("name");
+                                String email = person.getString("email");
+                                //JSONObject phone = person
+                                  //      .getJSONObject("phone");
+
+
+                                jsonResponse += "Name: " + name + "\n\n";
+                                jsonResponse += "Email: " + email + "\n\n";
+
+
+                            }
+
+                            txtResponse.setText(jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
 }
 
